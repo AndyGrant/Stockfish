780d6dc49497c822cb7d9a30a2ae5ef9dac48631
Andrew Grant <andrewgrant@gpu302.chess.com>
2024-07-08 12:23:43 -0700

    ...

--- src/evaluate.h
+++ src/evaluate.h
@@ -33,8 +33,8 @@ namespace Eval {
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
-#define EvalFileDefaultNameBig "nn-37f18f62d772.nnue"
-#define EvalFileDefaultNameSmall "nn-37f18f62d772.nnue"
+#define EvalFileDefaultNameBig "nn-ecb35f70ff2a.nnue"
+#define EvalFileDefaultNameSmall "nn-ecb35f70ff2a.nnue"
 
 namespace NNUE {
 struct Networks;
