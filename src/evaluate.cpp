058a1b3f7722eaa50ece37c549c491ea2ac6d336
andrew <andrew.github@grantnet.us>
2024-07-08 15:08:55 -0400

    ...

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -46,6 +46,9 @@ int Eval::simple_eval(const Position& pos, Color c) {
 }
 
 bool Eval::use_smallnet(const Position& pos) {
+
+    return false;
+
     int simpleEval = simple_eval(pos, pos.side_to_move());
     return std::abs(simpleEval) > 962;
 }
